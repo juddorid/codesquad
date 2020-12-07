eb7e6ae08bfbefe702b0f69dd3a110acb4343e92
Raccoon <nobllizdc@gmail.com>
2020-12-07 21:02:50 +0900

    평면 큐브 구현하기 뷰어 부분 완성
    * planeCube.js: 큐브 모양처럼 보이게 하기 위해서 색상을 추가해 놓았다.
    * step-2.html: codeSquadTest.html에서 버튼을 클릭했을 때 보이게 될 페이지이다.

--- step-2/planeCube.js
+++ step-2/planeCube.js
@@ -1,1 +1,58 @@
 // plane Cube
+const initialCube = [
+  ['R', 'R', 'W'],
+  ['G', 'C', 'W'],
+  ['G', 'B', 'B'],
+];
+
+const $containerBox = document.getElementById('container_box');
+const $colorValueBox = document.getElementsByClassName('color_box');
+const colorSet = { R: 'red', W: 'darkgray', G: 'green', C: 'cyan', B: 'blue' };
+
+const getCubeArray = function (arr) {
+  let cubeArray = [];
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      cubeArray.push(arr[i][j]);
+    }
+  }
+  return cubeArray;
+};
+
+const createWrapper = function () {
+  let tempDiv = document.createElement('div');
+  tempDiv.className = 'wrapper';
+  tempDiv.id = 'color_box_wrapper';
+  $containerBox.append(tempDiv);
+};
+
+const createColorBox = function () {
+  const $colorBoxWrapper = document.getElementById('color_box_wrapper');
+  let tempDiv = document.createElement('div');
+  tempDiv.className = 'color_box';
+  $colorBoxWrapper.append(tempDiv);
+};
+
+const getCubeBox = function () {
+  createWrapper();
+  for (let i = 0; i < 9; i++) {
+    createColorBox();
+  }
+};
+
+const inputColorValue = function () {
+  let cubeArray = getCubeArray(initialCube);
+  for (let i = 0; i < $colorValueBox.length; i++) {
+    $colorValueBox[i].innerText = cubeArray[i];
+  }
+};
+
+const getColor = function () {
+  for (let i = 0; i < $colorValueBox.length; i++) {
+    $colorValueBox[i].style.background = colorSet[$colorValueBox[i].innerText];
+  }
+};
+
+getCubeBox();
+inputColorValue();
+getColor();
