a6704aa3630155424e33135a1d8cea1fd2615625
Raccoon <nobllizdc@gmail.com>
2020-12-07 16:25:41 +0900

    단어 밀어내기 구현를 90% 구현
    * pushWord.js: 대부분의 기능을 구현했다. 사용자 입력값 예외 처리가 필요해 보이고, 커밋 중 더 좋아보이는 로직이 떠올랐다. 테스트 여유 시간에 따라서 수정해봐야겠다.
    * codeSquadTest.html: step-2, step-3를 한 브라우져에 출력할 수 있을 것 같아  step-1에 사용하려고 했던 pushWord.html 파일명을 변경했다.
    * pushWord.html: 파일명을 codeSquadTest.html로 변경했다.

--- step-1/pushWord.js
+++ step-1/pushWord.js
@@ -1,15 +1,94 @@
-const inputWord = 'apple';
+const $inputWord = document.querySelector('#step-1-input-word');
+const $inputDirection = document.querySelector('#step-1-input-direction');
+const $inputNumber = document.querySelector('#step-1-input-number');
+const $resultBox = document.querySelector('#step-1-input-result');
+const LEFT = 'L';
+const RIGHT = 'R';
+const PLUS = 'plus';
+const MINUS = 'minus';
 
-// L or R
-// Upper or Lowwer
-const direction = 'L';
+const operate = function (txt, dir, num) {
+  inputCheck(txt, dir, num);
 
-// -100 <= N < 100
-const inputNumber = 3;
-let wordArr = inputWord.split('');
-const UnifyText = (txt) => txt.toUpperCase();
-const checkNumber = (num) => (num > 0 ? 'plus' : 'minus');
-const checkedNumber = checkNumber(inputNumber);
-const checkDirection = function (txt, num) {};
-const goLeft = (arr) => arr.push(arr.shift());
-const goRight = (arr) => arr.unshift(arr.pop());
+  const checkNumber = (num) => (num > 0 ? PLUS : MINUS);
+
+  let unifiedDirection = dir.toUpperCase();
+  let checkedNumber = checkNumber(num);
+  let absNumber = Math.abs(num);
+  let lastDirection = getDirection(unifiedDirection, checkedNumber);
+  let lastWord = pushWord(txt, lastDirection, absNumber);
+  return ($resultBox.value = lastWord.join(''));
+};
+
+const inputCheck = function (txt, dir, num) {
+  console.log(txt, dir, num);
+  const spaceCheck = function (txt, dir, num) {
+    if (txt === '') {
+      alert('빈칸을 입력해주세요.');
+      moveFocus($inputWord);
+    } else if (dir === '') {
+      alert('빈칸을 입력해주세요.');
+      moveFocus($inputDirection);
+    } else if (num === '') {
+      alert('빈칸을 입력해주세요.');
+      moveFocus($inputNumber);
+    }
+  };
+  const directionCheck = function (dir) {
+    if (dir !== 'L' && dir !== 'l' && dir !== 'R' && dir !== 'r' && dir.length !== 0) {
+      alert('L 또는 R 만 입력해주세요. (소문자는 가능해요)');
+      moveFocus($inputDirection);
+      resetValue($inputDirection);
+    }
+  };
+  const numberCheck = function (num) {
+    if (num < -100 || num > 100) {
+      alert('-100보다 크거나 같고, 100보다 작은 수를 입력해주세요.');
+    }
+  };
+  spaceCheck(txt, dir, num);
+  directionCheck(dir);
+  numberCheck(num);
+};
+
+const getDirection = function (dir, num) {
+  let lastDirection;
+  if ((dir === LEFT && num === PLUS) || (dir === RIGHT && num === MINUS)) {
+    lastDirection = LEFT;
+  } else if ((dir === LEFT && num === MINUS) || (dir === RIGHT && num === PLUS)) {
+    lastDirection = RIGHT;
+  }
+  return lastDirection;
+};
+
+const pushWord = function (txt, dir, num) {
+  const goLeft = (arr) => arr.push(arr.shift());
+  const goRight = (arr) => arr.unshift(arr.pop());
+  let wordArr = txt.split('');
+  for (let i = 0; i < num; i++) {
+    if (dir === LEFT) {
+      goLeft(wordArr);
+    } else if (dir === RIGHT) {
+      goRight(wordArr);
+    }
+  }
+  return wordArr;
+};
+
+// auto tab
+function autoTab(current, next) {
+  if (current.value.length === 1) {
+    moveFocus(next);
+  }
+}
+
+// move focus
+function moveFocus(next) {
+  next.focus();
+}
+
+// reset value
+function resetValue(data) {
+  let resetData = (data.value = '');
+  return resetData;
+}
