a32631959271e4f5d13dbed10e0090878c9b0536
Raccoon <nobllizdc@gmail.com>
2020-12-08 18:11:24 +0900

    사용자 입력값 정리 완료
    * planeCube.js: input값을 받아서 큐브가 움직일 수 있도록 정리하였다.
    * step-2.html: 실행버튼 연결.

--- step-2/planeCube.js
+++ step-2/planeCube.js
@@ -9,21 +9,22 @@
 // > Q  Bye~를 출력하고 프로그램을 종료한다.
 
 // plane Cube
-const initialCube = [
+const myCube = [
   ['R', 'R', 'W'],
   ['G', 'C', 'W'],
   ['G', 'B', 'B'],
 ];
-
+const stringSet = ['U', "U'", 'R', "R'", 'L', "L'", 'B', "B'", 'Q'];
 const $containerBox = document.querySelector('#container_box');
 const $inputButton = document.querySelector('#input_btn');
+const $inputBox = document.querySelector('body > div > div.input_container > input.input_box');
 const WRAPPER = 'wrapper';
 const queryWRAPPER = '.wrapper';
 const queryCOLORBOX = '.color_box';
 let $wrapper;
 let wrapperCount = 0;
 
-getCube(initialCube, $containerBox);
+getCube(myCube, $containerBox);
 
 function getCube(cube, containerBox) {
   const getCubeArray = function (cube) {
@@ -81,25 +82,180 @@ function getCube(cube, containerBox) {
   wrapperCount++;
 }
 
+const currentCube = function (cube) {
+  let newCube = [];
+  for (let i = 0; i < cube.length; i++) {
+    for (let j = 0; j < cube.length; j++) {
+      newCube.push(cube[i][j]);
+    }
+  }
+  return newCube;
+};
+
 // U
 const upperLeft = function (currentArr) {
-  let upperArr = [currentArr[0][0], currentArr[0][1], currentArr[0][2]];
-  upperArr.push(upperArr.shift());
-  currentArr[0][0] = upperArr[0];
-  currentArr[0][1] = upperArr[1];
-  currentArr[0][2] = upperArr[2];
-  return currentArr;
+  let moveArr = [currentArr[0][0], currentArr[0][1], currentArr[0][2]];
+  moveArr.push(moveArr.shift());
+  for (let i = 0; i < currentArr.length; i++) {
+    currentArr[0][i] = moveArr[i];
+  }
+  getCube(myCube, $containerBox);
 };
 
 // U'
 const upperRight = function (currentArr) {
-  let upperArr = [currentArr[0][0], currentArr[0][1], currentArr[0][2]];
-  upperArr.push(upperArr.shift());
-  currentArr[0][0] = upperArr[0];
-  currentArr[0][1] = upperArr[1];
-  currentArr[0][2] = upperArr[2];
-  return currentArr;
+  let moveArr = [currentArr[0][0], currentArr[0][1], currentArr[0][2]];
+  moveArr.unshift(moveArr.pop());
+  for (let i = 0; i < currentArr.length; i++) {
+    currentArr[0][i] = moveArr[i];
+  }
+  getCube(myCube, $containerBox);
 };
 
-// let upLeft = upperLeft(initialCube);
-// getCube(upLeft, $outputContainerBox);
+// R
+const rightUp = function (currentArr) {
+  let moveArr = [currentArr[0][2], currentArr[1][2], currentArr[2][2]];
+  moveArr.push(moveArr.shift());
+  for (let i = 0; i < currentArr.length; i++) {
+    currentArr[i][2] = moveArr[i];
+  }
+  getCube(myCube, $containerBox);
+};
+
+// R'
+const rightDown = function (currentArr) {
+  let moveArr = [currentArr[0][2], currentArr[1][2], currentArr[2][2]];
+  moveArr.unshift(moveArr.pop());
+  for (let i = 0; i < currentArr.length; i++) {
+    currentArr[i][2] = moveArr[i];
+  }
+  getCube(myCube, $containerBox);
+};
+
+// L (warning! opposite direction!)
+const leftDown = function (currentArr) {
+  let moveArr = [currentArr[0][0], currentArr[1][0], currentArr[2][0]];
+  moveArr.unshift(moveArr.pop());
+  for (let i = 0; i < currentArr.length; i++) {
+    currentArr[i][0] = moveArr[i];
+  }
+  getCube(myCube, $containerBox);
+};
+
+// L' (warning! opposite direction!)
+const leftUp = function (currentArr) {
+  let moveArr = [currentArr[0][0], currentArr[1][0], currentArr[2][0]];
+  moveArr.push(moveArr.shift());
+  for (let i = 0; i < currentArr.length; i++) {
+    currentArr[i][0] = moveArr[i];
+  }
+  getCube(myCube, $containerBox);
+};
+
+// B
+const bottomRight = function (currentArr) {
+  let upperArr = [currentArr[2][0], currentArr[2][1], currentArr[2][2]];
+  upperArr.unshift(upperArr.pop());
+  for (let i = 0; i < currentArr.length; i++) {
+    currentArr[2][i] = upperArr[i];
+  }
+  getCube(myCube, $containerBox);
+};
+
+// B'
+const bottomLeft = function (currentArr) {
+  let upperArr = [currentArr[2][0], currentArr[2][1], currentArr[2][2]];
+  upperArr.push(upperArr.shift());
+  for (let i = 0; i < currentArr.length; i++) {
+    currentArr[2][i] = upperArr[i];
+  }
+  getCube(myCube, $containerBox);
+};
+
+// Q
+const quitBye = function () {
+  $inputBox.value = 'BYE~!';
+};
+
+// getInputValue
+const getInputValue = function () {
+  let inputValue = $inputBox.value;
+  let lastValue = valueCheck(inputValue);
+  return lastValue;
+};
+
+// U U' R R' L L' B B' Q
+//(singleQuote 정리)
+// 공백제거
+// 문자열 유효한지 검사
+const valueCheck = function (value) {
+  let valueArr = value.split('');
+  let upperValueArr = valueArr.map((e) => e.toUpperCase());
+  spaceCheck(upperValueArr);
+  let noSpaceArr = delSpace(upperValueArr);
+  checkSingleQuote(noSpaceArr);
+  let noSingleQuoteArr = delSingleQuote(noSpaceArr);
+  stringCheck(noSpaceArr);
+  // checkString(noSingleQuoteArr);
+
+  function spaceCheck(value) {
+    if (value.length === 0) {
+      alert('값을 입력해주세요.(빈칸입니다.)');
+      $inputBox.focus();
+    }
+  }
+  function delSpace(value) {
+    let noSpaceArr = [];
+    for (let i = 0; i < value.length; i++) {
+      if (value[i] !== ' ') {
+        noSpaceArr.push(value[i]);
+      }
+    }
+    return noSpaceArr;
+  }
+  function stringCheck(value) {
+    let count = 0;
+    for (let i = 0; i < value.length; i++) {
+      if (!stringSet.includes(value[i])) {
+        count++;
+      }
+    }
+    if (count !== 0) {
+      alert("알 수 없는 입력값이 있습니다.\n(유효한 입력값: U, U', R, R', L, L', B, B', Q)");
+      $inputBox.value = '';
+      $inputBox.focus();
+    }
+  }
+  function checkSingleQuote(value) {
+    let count = 0;
+    for (let i = 0; i < value.length; i++) {
+      if (value[i] === "'" && value[i - 1] === "'") {
+        count++;
+      }
+    }
+    if (count !== 0) {
+      alert('잘못된 입력입니다.(sigleQuote연속)');
+      $inputBox.value = '';
+      $inputBox.focus();
+    }
+    return value;
+  }
+  function delSingleQuote(value) {
+    let noSingleQuoteArr = [];
+    for (let i = 0; i < value.length; i++) {
+      if (value[i] === "'") {
+        noSingleQuoteArr.pop();
+        noSingleQuoteArr.push(value[i - 1] + value[i]);
+      } else {
+        noSingleQuoteArr.push(value[i]);
+      }
+    }
+    return noSingleQuoteArr;
+  }
+  return noSpaceArr;
+};
+
+// movePart
+const movingCube = function () {
+  let command = getInputValue();
+};
