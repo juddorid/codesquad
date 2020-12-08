6f6662818b300866deb97ec69e15711395c43d4c
Raccoon <nobllizdc@gmail.com>
2020-12-09 03:00:27 +0900

    루빅스 큐브 생성
    * rubiksCube.js
    : 6개의 색상을 가진 루빅스 큐브 배열 생성이 완료되었음.
    : 펼쳐진 큐브를 어떻게 표현할지 구상한 후 뷰어에 표현.
    
    * step-3.html
    : step-2의 틀을 비슷하게 가져와 step-3의 기본틀을 갖추었음.
    : 6면을 표현하고 계속해서 보여주어야 하기 때문에 작동 방식은 step-2와 같이하고, 표현 방식은 새로 구현.

--- step-3/rubiksCube.js
+++ step-3/rubiksCube.js
@@ -1,6 +1,20 @@
+const colorSet = ['B', 'W', 'O', 'G', 'Y', 'R'];
+const commandSet = ['F', 'R', 'U', 'B', 'L', 'D'];
 const RUBIKS = 3;
+const CONTAINER = 'line_container';
+const WRAPPER = 'wrapper';
+const COLORBOX = 'color_box';
 
-const getRubiksCube = function (color) {
+const $containerBox = document.querySelector('#container_box');
+const $inputButton = document.querySelector('#input_btn');
+const $inputBox = document.querySelector('body > div > div.input_container > input.input_box');
+let cubeCount = 0;
+
+let rubiksCube = getRubiksCube(colorSet);
+let lineFloor = createFloor($containerBox, CONTAINER);
+
+// plane cube
+function getRubiks(color) {
   let rubiks = [];
   for (let i = 0; i < RUBIKS; i++) {
     let temp = [];
@@ -10,6 +24,65 @@ const getRubiksCube = function (color) {
     rubiks.push(temp);
   }
   return rubiks;
-};
+}
 
-console.log(getRubiksCube('B'));
+// rubiks cube
+function getRubiksCube(set) {
+  let rubiksCube = [];
+  for (let i = 0; i < set.length; i++) {
+    rubiksCube.push(getRubiks(set[i]));
+  }
+  return rubiksCube;
+}
+
+// create div
+function createDIV(className) {
+  let tempDiv = document.createElement('div');
+  tempDiv.className = className;
+  return tempDiv;
+}
+
+// append container
+function appendDIV(where, what) {
+  let appendDiv = where.append(what);
+  return appendDiv;
+}
+
+// create floor for line container
+function createFloor(box, className) {
+  const FLOORS = 3;
+  for (let i = 0; i < FLOORS; i++) {
+    appendDIV(box, createDIV(className));
+  }
+}
+
+// create wrapper > and color box in line container
+function createCubeDOM() {
+  const boxSize = 9;
+  let lineFloor = createDIV(WRAPPER);
+  for (let i = 0; i < boxSize; i++) {
+    lineFloor.append(createDIV(COLORBOX));
+  }
+  return lineFloor;
+}
+
+function inputCubeValue(cube, containerBox) {
+  let cubeArray = getCubeArray(cube);
+  let colorBox = containerBox.children;
+  for (let i = 0; i < colorBox.length; i++) {
+    colorBox[i].innerText = cubeArray[i];
+  }
+}
+function getColor(containerBox) {
+  const colorSet = { B: 'brown', W: 'lightgray', O: 'orange', G: 'green', Y: 'yellow', R: 'red' };
+  let colorBox = containerBox.children;
+  for (let i = 0; i < colorBox.length; i++) {
+    colorBox[i].style.background = colorSet[colorBox[i].innerText];
+  }
+}
+
+// let box = createCubeDOM();
+// inputCubeValue(cube, box);
+// getColor(box);
+// containerBox.append(box);
+// wrapperCount++;
