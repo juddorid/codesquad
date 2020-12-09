f28f11ff107bc363d3c1e68d1206260377667e29
Raccoon <nobllizdc@gmail.com>
2020-12-09 13:31:00 +0900

    큐브 뷰어 박스 구조조정
    * rubiksCube.js
    : 큐브를 반복해서 그려 넣으려고 하니 복잡해져서, 박스에 넣어서 쉽게 만들기 위해서 view_container에 drawCube를 담았다.
    : 함수 안에 데이터를 넣으려고 하니 drawCube에 넣었던 함수가 다시 필요한걸 깨닫고, 다시 꺼내놓았다.
    : 초기 큐브를 만들어 내는 함수는 읽기 쉽게 하기 위해서 평면 큐브 만드는 함수를 육면체 큐브로 만드는 함수 안에 포함시켜 놓았다.
    
    *step-3.html
    : 뷰어 박스 구조를 조정하기 위해 className과 id 변경했다.

--- step-3/rubiksCube.js
+++ step-3/rubiksCube.js
@@ -2,34 +2,34 @@ const colorSet = ['B', 'W', 'O', 'G', 'Y', 'R'];
 const commandSet = ['F', 'R', 'U', 'B', 'L', 'D'];
 const RUBIKS = 3;
 const MIDDLE = 4;
-const CONTAINER = 'line_container';
+const VIEW_CONTAINER = 'view_container';
+const LINE_CONTAINER = 'line_container';
 const WRAPPER = 'wrapper';
 const COLORBOX = 'color_box';
 
-const $viewBox = document.querySelector('#container_box');
+const $outputBox = document.querySelector('#output_box');
 const $inputButton = document.querySelector('#input_btn');
 const $inputBox = document.querySelector('body > div > div.input_container > input.input_box');
 let cubeCount = 0;
 
 let rubiksCube = getRubiksCube(colorSet);
-
-let initialCube = drawCube($viewBox, CONTAINER);
-
-// plane cube
-function getRubiks(color) {
-  let rubiks = [];
-  for (let i = 0; i < RUBIKS; i++) {
-    let temp = [];
-    for (let j = 0; j < RUBIKS; j++) {
-      temp.push(color);
-    }
-    rubiks.push(temp);
-  }
-  return rubiks;
-}
+let boxes = inputCubeBox($outputBox, VIEW_CONTAINER);
+let cube = drawCube(boxes, LINE_CONTAINER);
 
 // rubiks cube
 function getRubiksCube(set) {
+  // plane cube
+  function getRubiks(color) {
+    let rubiks = [];
+    for (let i = 0; i < RUBIKS; i++) {
+      let temp = [];
+      for (let j = 0; j < RUBIKS; j++) {
+        temp.push(color);
+      }
+      rubiks.push(temp);
+    }
+    return rubiks;
+  }
   let rubiksCube = [];
   for (let i = 0; i < set.length; i++) {
     rubiksCube.push(getRubiks(set[i]));
@@ -37,23 +37,15 @@ function getRubiksCube(set) {
   return rubiksCube;
 }
 
+// input cube in the box
+function inputCubeBox(box, className) {
+  let container = createDIV(className);
+  box.append(container);
+  return container;
+}
+
 // drawing Cube
 function drawCube(box, className) {
-  // create div
-  function createDIV(className) {
-    let tempDiv = document.createElement('div');
-    tempDiv.className = className;
-    return tempDiv;
-  }
-  // create wrapper > and color box in line container
-  function createCubeDOM() {
-    const boxSize = 9;
-    let wrapper = createDIV(WRAPPER);
-    for (let i = 0; i < boxSize; i++) {
-      wrapper.append(createDIV(COLORBOX));
-    }
-    return wrapper;
-  }
   function oneWrapper() {
     let container = createDIV(className);
     container.append(createCubeDOM());
@@ -66,9 +58,37 @@ function drawCube(box, className) {
       box.append(container);
     }
   }
-  oneWrapper($viewBox, CONTAINER);
-  middleWrapper($viewBox, CONTAINER);
-  oneWrapper($viewBox, CONTAINER);
+  oneWrapper();
+  middleWrapper();
+  oneWrapper();
+}
+// create div
+function createDIV(className) {
+  let tempDiv = document.createElement('div');
+  tempDiv.className = className;
+  return tempDiv;
+}
+// create wrapper > and color box in line container
+function createCubeDOM() {
+  const boxSize = 9;
+  let wrapper = createDIV(WRAPPER);
+  for (let i = 0; i < boxSize; i++) {
+    wrapper.append(createDIV(COLORBOX));
+  }
+  return wrapper;
+}
+
+// get Cube Array
+function getCubeArray(cube) {
+  let cubeArray = [];
+  for (let i = 0; i < cube.length; i++) {
+    for (let j = 0; j < cube[i].length; j++) {
+      for (let k = 0; k < cube[i][j].length; k++) {
+        cubeArray.push(cube[i][j][k]);
+      }
+    }
+  }
+  return cubeArray;
 }
 
 // input cube value
@@ -79,6 +99,7 @@ function inputCubeValue(cube, containerBox) {
     colorBox[i].innerText = cubeArray[i];
   }
 }
+
 function getColor(containerBox) {
   const colorSet = { B: 'brown', W: 'lightgray', O: 'orange', G: 'green', Y: 'yellow', R: 'red' };
   let colorBox = containerBox.children;
